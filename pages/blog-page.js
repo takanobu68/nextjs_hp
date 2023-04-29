c4bfcf55569dda1ce3d583dec2cca87d06a81248
Takanobu <61861946+takanobu68@users.noreply.github.com>
2023-04-29 12:22:04 +0900

    getStaticProps

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,7 +1,27 @@
 import Layout from '../components/Layout';
+import Post from '../components/Post';
+import { getAllPostsData } from '../lib/posts';
 
-const Blog = () => {
-  return <Layout>Blog Page</Layout>;
+const Blog = ({ posts }) => {
+  return (
+    <Layout>
+      <ul className='m-10'>
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </ul>
+    </Layout>
+  );
 };
 
 export default Blog;
+
+export async function getStaticProps() {
+  const posts = await getAllPostsData();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
