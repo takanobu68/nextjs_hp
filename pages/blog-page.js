20bb000ed99c717479a310ec4a398bcd1c406e30
Takanobu <61861946+takanobu68@users.noreply.github.com>
2023-04-29 10:48:40 +0900

    Layoutコンポーネントの作成と各ページへの適用

---
+++ pages/blog-page.js
@@ -0,0 +1,7 @@
+import Layout from '../components/Layout';
+
+const Blog = () => {
+  return <Layout>Blog Page</Layout>;
+};
+
+export default Blog;
---
+++ pages/contact-page.js
@@ -0,0 +1,7 @@
+import Layout from '../components/Layout';
+
+const Contact = () => {
+  return <Layout>Contact Page</Layout>;
+};
+
+export default Contact;
